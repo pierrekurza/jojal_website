77e63d3c5db1e84e3541e9e64656546ba88b6296
XeloOw <pierrekurzawski@gmail.com>
2017-07-05 11:03:45 +0200

    Base

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: XeloOw
+ * Date: 05-07-17
+ * Time: 11h01
+ */
+
+namespace App\Controller;
+
+
+class HomeController extends AppController
+{
+    public function isAuthorized()
+    {
+
+    }
+}
---
+++ src/Template/Home/index.ctp
@@ -0,0 +1,1 @@
+<h1>Bonjour PAGE D'ACCUEIL</h1>
